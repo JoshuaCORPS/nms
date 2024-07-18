2b311d092c740166c9c52e9e91a26802a799606f
corpsss <joshua.corpin@lgusuite.com>
2024-07-18 14:08:35 +0800

    Initial commit

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: 1935, // RTMP port remains static as Heroku does not dynamically assign this
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
   },
   http: {
-    port: 8000,
+    port: process.env.PORT || 8000, // HTTP server binds to the port specified by the $PORT environment variable
     allow_origin: "*",
   },
 };
