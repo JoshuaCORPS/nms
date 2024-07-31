dd7032c4766b41546bf27e2031f1e59db58941cf
corpsss <joshua.corpin@lgusuite.com>
2024-07-31 11:11:53 +0800

    removed media root

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ const config = {
     port: process.env.PORT || 8000, // HTTP server binds to the port specified by the $PORT environment variable
     allow_origin: "*",
   },
-  trans: {
-    ffmpeg: "/usr/local/bin/ffmpeg", // Ensure ffmpeg is installed and provide the correct path
-    tasks: [
-      {
-        app: "live",
-        hls: true,
-        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-        dash: true,
-        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-      },
-    ],
-  },
-  mediaRoot: path.resolve("/var/media"),
+  // trans: {
+  //   ffmpeg: "/usr/local/bin/ffmpeg", // Ensure ffmpeg is installed and provide the correct path
+  //   tasks: [
+  //     {
+  //       app: "live",
+  //       hls: true,
+  //       hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+  //       dash: true,
+  //       dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+  //     },
+  //   ],
+  // },
+  // mediaRoot: path.resolve("/var/media"),
 };
 
 // Ensure the directory exists and is writable
